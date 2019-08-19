4638ebaaaba27bb483c1960b8f33799b34fc74bc
christianaichner <contact@aichner-christian.com>
2019-08-19 02:51:53 +0200

    Improve call to action
    Images and Parallax have been implemented.

--- src/components/organisms/sections/CallToAction/index.jsx
+++ src/components/organisms/sections/CallToAction/index.jsx
@@ -31,6 +31,10 @@ import layer1 from '../../../../assets/bg/l1.png';
 import layer2 from '../../../../assets/bg/l2.png';
 import layer3 from '../../../../assets/bg/l3.png';
 import layer4 from '../../../../assets/bg/l4.png';
+// Rocket
+import rocket from '../../../../assets/content/icons/rocket.png';
+import rocket_pen from '../../../../assets/content/icons/rocket_pen.png';
+import planet1 from '../../../../assets/content/icons/planet1.jpg';
 
 
 class CallToAction extends React.Component {
@@ -39,9 +43,59 @@ class CallToAction extends React.Component {
             <div id="calltoaction" className={this.props.globalStore.mode ? ("dark") : ("light")}>
                 <MDBRow>
                     <MDBCol className="text-center">
+                        {/*<ParallaxBanner
+                        className="hero-parallax"
+                        layers={[
+                            {
+                                image: rocket_pen,
+                                amount: 0.0,
+                                expanded: true
+                            },
+                            {
+                                image: rocket,
+                                amount: -0.4,
+                                expanded: true
+                            },
+                        ]}
+                        />*/}
+                        <MDBRow className="space-left">
+                            <MDBCol>
+                                <MDBView>
+                                    <Parallax className="planet1" x={[60, 0]} tagOuter="figure">
+                                        <img className="rotate" src={planet1} alt="Moon" />
+                                    </Parallax>
+                                <MDBMask />
+                                </MDBView>
+                            </MDBCol>
+                            <MDBCol>
+                                <MDBView>
+                                    <Parallax className="rocket" y={[60, -60]} tagOuter="figure">
+                                        <img className="fly" src={rocket} alt="Rocket" />
+                                    </Parallax>
+                                    <MDBMask />
+                                </MDBView>
+                            </MDBCol>
+                            <MDBCol>
+
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBCol>
+                    <MDBCol md="auto" className="text-center">
                         <h2 className="gidole">Es ist einfach zu starten!</h2>
                         <p>Und der Einstieg ist kostenlos. Zwei Dinge die jeder liebt.</p>
-                        <MDBBtn color="agency-red" size="lg">Jetzt starten</MDBBtn>
+                        <MDBBtn color="agency-red" size="lg"><MDBIcon icon="rocket" className="pr-2"/>Jetzt starten</MDBBtn>
+                    </MDBCol>
+                    <MDBCol>
+                        <MDBRow className="space-right">
+                            <MDBCol>
+                                <MDBView>
+                                <Parallax className="planet2" y={[60, 0]} tagOuter="figure">
+                                    <img className="rotate" src={planet1} alt="Moon" />
+                                </Parallax>
+                                <MDBMask />
+                                </MDBView>
+                            </MDBCol>
+                        </MDBRow>
                     </MDBCol>
                 </MDBRow>
             </div>
