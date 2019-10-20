0c06c82e03ad653d25ae344058f467a799a61b58
Christian Aichner <contact@aichner-christian.com>
2019-10-20 19:06:02 +0200

    Add details
    Details to branding information has been added.

--- src/components/pages/BrandingPage/index.jsx
+++ src/components/pages/BrandingPage/index.jsx
@@ -19,7 +19,7 @@ import './branding.scss';
 
 //> Images
 // Logo
-import { ReactComponent as Logo } from  '../../../assets/content/logo_main.svg';
+import { ReactComponent as Logo } from  '../../../assets/content/logo_full.svg';
 // Weddings logo
 import weddingsLogo from '../../../assets/content/weddings.png';
 
@@ -33,7 +33,7 @@ class BrandingPage extends React.Component {
             <MDBCard>
               <MDBCardBody>
                 <p className="lead">Logo (on light)</p>
-                <Logo />
+                <Logo className="logo-light"/>
                 <div>
                   <MDBBtn
                   color="elegant"
@@ -93,6 +93,110 @@ class BrandingPage extends React.Component {
                 </div>
               </MDBCardBody>
             </MDBCard>
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="mt-5">
+          <MDBCol md="6">
+            <p className="lead font-weight-bold">Mach diese tollen Dinge</p>
+            <ul>
+              <li>
+                <p className="mt-2">
+                  <MDBIcon icon="check" className="green-text pr-2" />
+                  Verwende unser Logo um auf uns zu verlinken
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="check" className="green-text pr-2" />
+                  Verwende unser Logo in Deinem Blog oder Nachrichtenartikel über uns
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="check" className="green-text pr-2" />
+                  Verwende unser Logo um auf Zusammenarbeit hinzuweisen
+                </p>
+              </li>
+            </ul>
+          </MDBCol>
+          <MDBCol md="6">
+            <p className="lead font-weight-bold">Bitte tu diese Dinge nicht</p>
+            <ul>
+              <li>
+                <p>
+                  <MDBIcon icon="times" className="red-text pr-2" />
+                  Verwendung des Logos für eigene Zwecke
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="times" className="red-text pr-2" />
+                  Erstellung einer abgeänderten / modifizierten Version des Logos
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="times" className="red-text pr-2" />
+                  Einbettung des Logos in Dein eigenes Logo
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="times" className="red-text pr-2" />
+                  Verwendung jeglicher Schaffungen der Agentur ohne Erlaubnis
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="times" className="red-text pr-2" />
+                  Verkauf jeglicher Schaffungen der Agentur ohne Erlaubnis
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="times" className="red-text pr-2" />
+                  Jegliche Veränderung des Logos (z.B. Farben, Maße, Text, ...)
+                </p>
+              </li>
+              <li>
+                <p>
+                  <MDBIcon icon="times" className="red-text pr-2" />
+                  Einbettung des Logos auf unpassendem Hintergrund (z.B. Dunkles Logo auf dunklem Hintergrund)
+                </p>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+        <hr />
+        <MDBRow>
+          <MDBCol md="6">
+            <p className="lead font-weight-bold">Bitte kontaktiere uns</p>
+            <p>wenn Du unsere Schaffungen verwenden willst oder Du diese in einem Video oder 
+            Mainstream-Medien verwenden willst.</p>
+            <MDBBtn
+            color="elegant"
+            size="md"
+            rounded
+            tag="a"
+            href="mailto:contact@aichner-christian.com"
+            >
+              Kontakt
+            </MDBBtn>
+          </MDBCol>
+          <MDBCol md="6">
+            <p className="lead font-weight-bold">Du willst über uns berichten?</p>
+            <p>Nutze gerne unsere Ressourcen unter Berücksichtung der oben angeführten Regeln.</p>
+            <p className="lead font-weight-bold">Du benötigst Auskunft?</p>
+            <p>Über firmeninterne Details können wir Dir für deinen Bericht / Blog / Artikel gerne Auskunft geben.</p>
+            <MDBBtn
+            color="elegant"
+            size="md"
+            rounded
+            tag="a"
+            href="mailto:contact@aichner-christian.com"
+            >
+              Kontakt
+            </MDBBtn>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
