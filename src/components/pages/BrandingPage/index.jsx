c703d9d81205403b3eb86661f280f484c23934a2
Christian Aichner <contact@aichner-christian.com>
2019-10-20 18:39:17 +0200

    Add logo examples
    Three logo examples have been added. The download
    buttons do not yet work.

--- src/components/pages/BrandingPage/index.jsx
+++ src/components/pages/BrandingPage/index.jsx
@@ -10,22 +10,90 @@ import {
   MDBCol,
   MDBCard,
   MDBCardBody,
+  MDBBtn,
+  MDBIcon,
 } from 'mdbreact';
+
+//> CSS
+import './branding.scss';
+
+//> Images
+// Logo
+import { ReactComponent as Logo } from  '../../../assets/content/logo_main.svg';
+// Weddings logo
+import weddingsLogo from '../../../assets/content/weddings.png';
 
 class BrandingPage extends React.Component {
   render() {
     return (
-      <MDBContainer>
-        <h2 className="text-center">Logos und Verwendung</h2>
-        <MDBRow>
+      <MDBContainer id="branding" className="py-5">
+        <h2 className="text-center mb-5">Logos und Verwendung</h2>
+        <MDBRow className="text-center">
           <MDBCol md="4">
             <MDBCard>
               <MDBCardBody>
-                <p>Test</p>
+                <p className="lead">Logo (on light)</p>
+                <Logo />
+                <div>
+                  <MDBBtn
+                  color="elegant"
+                  rounded
+                  >
+                  <MDBIcon icon="download" className="pr-2" />
+                  .svg
+                  </MDBBtn>
+                  <MDBBtn
+                  color="elegant"
+                  rounded
+                  >
+                  <MDBIcon icon="download" className="pr-2" />
+                  .png
+                  </MDBBtn>
+                </div>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
-
+          <MDBCol md="4">
+            <MDBCard>
+              <MDBCardBody className="elegant-color white-text">
+                <p className="lead">Logo (on dark)</p>
+                <Logo className="logo-dark"/>
+                <div>
+                  <MDBBtn
+                  color="white"
+                  rounded
+                  >
+                  <MDBIcon icon="download" className="pr-2" />
+                  .svg
+                  </MDBBtn>
+                  <MDBBtn
+                  color="white"
+                  rounded
+                  >
+                  <MDBIcon icon="download" className="pr-2" />
+                  .png
+                  </MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md="4">
+            <MDBCard>
+              <MDBCardBody className="elegant-color white-text">
+                <p className="lead">Wedding Logo (universal)</p>
+                <img className="img-fluid" src={weddingsLogo} alt="Weddings logo"/>
+                <div>
+                  <MDBBtn
+                  color="white"
+                  rounded
+                  >
+                  <MDBIcon icon="download" className="pr-2" />
+                  .png
+                  </MDBBtn>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
         </MDBRow>
       </MDBContainer>
     );
