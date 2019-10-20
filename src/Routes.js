1f9e7dcaf0a0fb8fbdee4299850d45dc510156bb
Christian Aichner <contact@aichner-christian.com>
2019-10-20 20:09:33 +0200

    Add commentary
    Commentary about the component page in the Routes
    file has been added.

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,7 @@ import { Route, Switch } from 'react-router-dom';
 //> Components
 /**
  * HomePage: The start page
+ * BrandingPage: A page telling how to use our logos and branding
  * BlogPage: Our blog (based on Google Blogger)
  * BlogListPage: An overview of all our blogs (based on Google Blogger)
  * BlogListPageLabel: An overview of all blogs sorted by a label (based on Google Blogger)
