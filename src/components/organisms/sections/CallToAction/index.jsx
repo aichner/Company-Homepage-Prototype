9b2331e8726509e823a24534e8708cc48fb50460
Aichnerc <contact@aichner-christian.com>
2019-11-09 19:37:57 +0100

    Improve code quality
    The code quality has been improved by adding a
    useless <FadeIn> and enforcing the 119 chars per
    line rule.

--- src/components/organisms/sections/CallToAction/index.jsx
+++ src/components/organisms/sections/CallToAction/index.jsx
@@ -30,51 +30,52 @@ class CallToAction extends React.Component {
     render() {
         const { globalstore } = this.props;
         return (
-            <FadeIn>
-                <div id="calltoaction" className={globalstore.mode ? ("dark") : ("light")}>
-                    <MDBRow>
-                        <MDBCol className="text-center">
-                            <MDBRow className="space-left">
-                                <MDBCol>
-                                    <MDBView>
-                                        <Parallax className="planet1" x={[60, 0]} tagOuter="figure">
-                                            <img className="rotate" src={planet1} alt="Moon" />
-                                        </Parallax>
-                                    <MDBMask />
-                                    </MDBView>
-                                </MDBCol>
-                                <MDBCol>
-                                    <MDBView>
-                                        <Parallax className="rocket" y={[60, -60]} tagOuter="figure">
-                                            <img className="fly" src={rocket} alt="Rocket" />
-                                        </Parallax>
-                                        <MDBMask />
-                                    </MDBView>
-                                </MDBCol>
-                                <MDBCol>
-                                </MDBCol>
-                            </MDBRow>
-                        </MDBCol>
-                        <MDBCol md="auto" className="text-center">
-                            <h2 className="gidole">Es ist einfach <strong>jetzt</strong> zu starten!</h2>
-                            <p>Und der Einstieg ist kostenlos. Zwei Dinge die jeder liebt.</p>
-                            <MDBBtn color="agency-red" size="lg"><MDBIcon icon="rocket" className="pr-2"/>Jetzt starten</MDBBtn>
-                        </MDBCol>
-                        <MDBCol>
-                            <MDBRow className="space-right">
-                                <MDBCol>
-                                    <MDBView>
-                                    <Parallax className="planet2" y={[60, 0]} tagOuter="figure">
+            <div id="calltoaction" className={globalstore.mode ? ("dark") : ("light")}>
+                <MDBRow>
+                    <MDBCol className="text-center">
+                        <MDBRow className="space-left">
+                            <MDBCol>
+                                <MDBView>
+                                    <Parallax className="planet1" x={[60, 0]} tagOuter="figure">
                                         <img className="rotate" src={planet1} alt="Moon" />
                                     </Parallax>
+                                <MDBMask />
+                                </MDBView>
+                            </MDBCol>
+                            <MDBCol>
+                                <MDBView>
+                                    <Parallax className="rocket" y={[60, -60]} tagOuter="figure">
+                                        <img className="fly" src={rocket} alt="Rocket" />
+                                    </Parallax>
                                     <MDBMask />
-                                    </MDBView>
-                                </MDBCol>
-                            </MDBRow>
-                        </MDBCol>
-                    </MDBRow>
-                </div>
-            </FadeIn>
+                                </MDBView>
+                            </MDBCol>
+                            <MDBCol>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBCol>
+                    <MDBCol md="auto" className="text-center">
+                        <h2 className="gidole">Es ist einfach <strong>jetzt</strong> zu starten!</h2>
+                        <p>Und der Einstieg ist kostenlos. Zwei Dinge die jeder liebt.</p>
+                        <MDBBtn color="agency-red" size="lg">
+                        <MDBIcon icon="rocket" className="pr-2"/>
+                        Jetzt starten
+                        </MDBBtn>
+                    </MDBCol>
+                    <MDBCol>
+                        <MDBRow className="space-right">
+                            <MDBCol>
+                                <MDBView>
+                                <Parallax className="planet2" y={[60, 0]} tagOuter="figure">
+                                    <img className="rotate" src={planet1} alt="Moon" />
+                                </Parallax>
+                                <MDBMask />
+                                </MDBView>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBCol>
+                </MDBRow>
+            </div>
         );
     }
 }
